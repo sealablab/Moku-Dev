9c8544cf7f40a719ca61a04f1901e4b6ecb4dd73
johnny cache <johnycsh@DRP-e1.local>
2025-05-11 11:17:46 -0700

    Adding the (generic) enabled blinker. will come back for the basic

---
+++ HDL/Blinkers/Top.vhdl
@@ -0,0 +1,19 @@
+-- Blinkers/Top.vhd 
+
+library IEEE;
+use IEEE.Std_Logic_1164.all;
+use IEEE.Numeric_Std.all;
+
+
+architecture Behavioural of CustomWrapper is
+begin
+
+  u_blink: entity work.blink
+    port map (
+      clk       => Clk,
+      reset     => Reset,
+      control0  => Control0,
+      blink_out => OutputA
+    );
+
+end architecture Behavioural;
